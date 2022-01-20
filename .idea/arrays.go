9e0132a238fe9a46cb210acc6481af7a839d2152
Confidence Okere <okerenomso@gmail.com>
2022-01-20 14:41:14 +0100

    Some Practices on Go. Arrays, Slicing and appending an array.

--- .idea/arrays.go
+++ .idea/arrays.go
@@ -73,23 +73,44 @@ func main() {
 	*/
 
 	//Example8:Using the Slice function suing the MAKE keyword in an array:
-	/*	numbe:= []int{2,4,6,8,10}
-		slice1 := make([]int, 3)		//Creates a new int array of length 3
-		copy(slice1, numbe)  			//this copies the values of the oll array into the new array.
-		fmt.Println(numbe, slice1)
+	/*		numbe:= []int{2,4,6,8,10}
+			slice1 := make([]int, 3)		//Creates a new int array of length 3
+			copy(slice1, numbe)  			//this copies the values of the oll array into the new array.
+			fmt.Println(numbe, slice1)
 	*/
 
 	//Example9: Still on slicing and copying an array element into a new array,finding the sum of the new array, appenindind new elements into the new array:
-	array := []int{20, 40, 60, 80, 100}
-	array2 := make([]int, 4)
-	copy(array2, array)
-	array3 := append(array2, 100, 120, 140)
-	//finding the sum of the arry2
-	var sum int
-	for _, value := range array2 {
-		sum += value
-	}
-	fmt.Println(array, array2, "The sum of array2 is ", sum, array3)
+	/*	array := []int{20, 40, 60, 80, 100}
+		array2 := make([]int, 4)
+		copy(array2, array)
+		array3 := append(array2, 100, 120, 140)
+		//finding the sum of the arry2
+		var sum int
+		for _, value := range array2 {
+			sum += value
+		}
+		fmt.Println(array, array2, "The sum of array2 is ", sum, array3)
+	*/
 
-	//Example10:
+	//Example10: Given the array below: find arry[2:5]?
+	/*	arry:=[6]string{"a","b","c","d","e","f"}
+		fmt.Println(arry[2:5])
+	*/
+
+	//Example11: find the smallest number in this list.
+	figu := []int{48, 96, 86, 68, 57, 82, 63, 70, 37, 34, 83, 27, 19, 97, 9, 17}
+	smallest := 100
+	largest := 0
+	for i := 0; i <= len(figu)-1; i++ {
+		if figu[i] < smallest {
+			smallest = figu[i]
+		}
+		if figu[i] > largest {
+			largest = figu[i]
+		}
+	}
+	fmt.Println(figu)
+	fmt.Println("The smallest number is ", smallest)
+	fmt.Println("The largest is ", largest)
+
 }
